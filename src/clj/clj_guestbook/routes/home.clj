195154c48925db6c0e0f7e609c5492d3e2ce243f
garage404 <38818825+pozorfluo@users.noreply.github.com>
2020-10-15 22:15:00 +0200

    Add message validation

--- src/clj/clj_guestbook/routes/home.clj
+++ src/clj/clj_guestbook/routes/home.clj
@@ -5,18 +5,37 @@
    [clojure.java.io :as io]
    [clj-guestbook.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [struct.core :as st]))
 
-(defn home-page [request]
+(defn home-page [{:keys [flash] :as request}]
 ;   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
-  (layout/render request "home.html" {:messages (db/get-messages)}))
+  (layout/render request "home.html" (merge {:messages (db/get-messages)} 
+        (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
-  (layout/render request "about.html"))
+    (layout/render request "about.html"))
+
+(def message-schema
+  [[:name
+    st/required 
+    st/string]
+   [:message
+    st/required
+    st/string
+    {:message "Your message must be at least 10 characters long !"
+     :validate (fn [msg] (>= (count msg) 10))}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
 
 (defn save-message! [{:keys [params]}]
-  (db/save-message! params)
-  (response/found "/"))
+    (if-let [errors (validate-message params)]
+        (-> (response/found "/")
+        (assoc ,,, :flash (assoc params :errors errors)))
+    (do
+        (db/save-message! params)
+        (response/found "/"))))
 
 (defn home-routes []
   [""
